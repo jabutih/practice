090a869dc9b87046f37badd075065836b9e0092b
Jabuti <guilhermeoliveirfm@gmail.com>
2025-01-10 16:20:55 -0300

    atualização

--- calculadora.c
+++ calculadora.c
@@ -4,23 +4,35 @@
 
 int main(void)
 {
-    int primeiro = get_int("Insira um valor:");
+    while (true) {
+            float primeiro = get_float("Insira um valor:");
     string operador = get_string("Insira um operador (+, -, /, *):");
-    int segundo = get_int("Insira outro valor:");
+    float segundo = get_float("Insira outro valor:");
 
     if (strcmp(operador, "+") == 0) {
-        printf("O resultado é %i\n", primeiro + segundo);
+        printf("O resultado é %f\n", primeiro + segundo);
     }
     else if (strcmp(operador, "-") == 0) {
-        printf("O resultado é %i\n", primeiro - segundo);
+        printf("O resultado é %f\n", primeiro - segundo);
     }
     else if (strcmp(operador, "/") == 0) {
-        printf("O resultado é %i\n", primeiro / segundo);
+        if (segundo == 0) {
+            printf("Erro, divisão por zero não é permitida.\n");
+        }
+        else {
+            printf("O resultado é %f\n", primeiro / segundo);
+        }
     }
     else if (strcmp(operador, "*") == 0) {
-        printf("O resultado é %i\n", primeiro * segundo);
+        printf("O resultado é %f\n", primeiro * segundo);
     }
     else {
         printf("O operador inserido é inválido.\n");
+    }
+
+    string continuar = get_string("Deseja fazer outro cálculo [S/N]?");
+    if (strcmp(continuar, "n") == 0 || strcmp(continuar, "N") == 0) {
+        break;
+    }
     }
 }
