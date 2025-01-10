b530e1de874e838ac9b43630b6ef57da6fc46567
Jabuti <guilhermeoliveirfm@gmail.com>
2025-01-10 13:18:56 -0300

    changes

--- C/calculadora.c
+++ C/calculadora.c
@@ -4,35 +4,20 @@
 
 int main(void)
 {
-    while (true) {
-    float primeiro = get_float("Insira um valor:");
+    int primeiro = get_int("Insira um valor:");
     string operador = get_string("Insira um operador (+, -, /, *):");
-    float segundo = get_float("Insira outro valor:");
+    int segundo = get_int("Insira outro valor:");
 
     if (strcmp(operador, "+") == 0) {
-        printf("O resultado é %f\n", primeiro + segundo);
+        printf("O resultado é %i\n", primeiro + segundo);
     }
     else if (strcmp(operador, "-") == 0) {
-        printf("O resultado é %f\n", primeiro - segundo);
+        printf("O resultado é %i\n", primeiro - segundo);
     }
     else if (strcmp(operador, "/") == 0) {
-        if (segundo == 0) {
-            printf("Erro, divisão por zero não é permitida.\n");
-        }
-        else {
-            printf("O resultado é %f\n", primeiro / segundo);
-        }
+        printf("O resultado é %i\n", primeiro / segundo);
     }
     else if (strcmp(operador, "*") == 0) {
-        printf("O resultado é %f\n", primeiro * segundo);
-    }
-    else {
-        printf("O operador inserido é inválido.\n");
-    }
-
-    string continuar = get_string("Deseja fazer outro cálculo [S/N]?");
-    if (strcmp(continuar, "n") == 0 || strcmp(continuar, "N") == 0) {
-        break;
-    }
+        printf("O resultado é %i\n", primeiro * segundo);
     }
 }
--- C/hello.c
+++ C/hello.c
@@ -12,3 +12,40 @@ int main(void)
     // \n - creates a new line/line break
     // ; - finishes the line of code
 }
+
+// side notes
+
+// strings - basically text, sequences of characters
+
+// \ - more escape sequences
+    // \r - goes to the beginning of the line
+    // \" - makes double quote
+    // \' - makes single quotes
+    // \\ - makes backslashes
+
+// bool - true or false
+// char - single characters
+// double and float - real numbers (with decimal points)
+// long - long integers
+
+// format codes - basically placeholders
+    // %s - asking to 'put another string in the following'
+    // %i - for integers
+
+// conditionals - decide which way to go based on a question
+    // if
+    // else
+    // else if - else and then if conditional
+
+// operators
+    // = - assign a return value from the right side to the left side
+    // < - less than
+    // <= - less than or equal
+    // > - greater than
+    // >= - greater than or equal
+    // == - equal
+    // != - different
+
+// variables
+
+    // when declaring variables, define their type → int numero = 4
