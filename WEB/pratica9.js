c2aa487da10f52c1bf3fb042d22a3c400f81140a
Jabuti <guilhermeoliveirfm@gmail.com>
2025-02-25 01:02:48 -0300

    .

--- WEB/pratica9.js
+++ WEB/pratica9.js
@@ -1,12 +1,10 @@
 const campo1 = document.querySelector("#valor1");
 const campo2 = document.querySelector("#valor2");
 const seletor = document.querySelector("#operacao");
+const botao = document.querySelector("#botao");
 let resposta = document.querySelector("#resposta");
 
-seletor.addEventListener("change", calcular);
-
-campo1.addEventListener("keyup", calcular);
-campo2.addEventListener("keyup", calcular);
+botao.addEventListener("click", calcular);
 
 function calcular() {
     const valor1 = parseInt(campo1.value);
@@ -14,22 +12,17 @@ function calcular() {
 
     const operacao = seletor.value;
 
-    if (campo1.value === '' || campo2.value === '') {
-        resposta.innerHTML="Preencha os campos, por favor.";
+    if (operacao === "somar") {
+        resposta.innerHTML = valor1 + valor2;
+    }
+    if (operacao === "subtrair") {
+        resposta.innerHTML = valor1 - valor2;
+    }
+    if (operacao === "dividir") {
+        resposta.innerHTML = valor1 / valor2;
+    }
+    if (operacao === "multiplicar") {
+        resposta.innerHTML = valor1 * valor2;
     }
 
-    else {
-        if (operacao === "somar") {
-            resposta.innerHTML = valor1 + valor2;
-        }
-        if (operacao === "subtrair") {
-            resposta.innerHTML = valor1 - valor2;
-        }
-        if (operacao === "dividir") {
-            resposta.innerHTML = valor1 / valor2;
-        }
-        if (operacao === "multiplicar") {
-            resposta.innerHTML = valor1 * valor2;
-        }
-    }
 }
