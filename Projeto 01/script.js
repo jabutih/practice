40bab5236f3a4d5bc206ce8fd66f8f628bc02b7f
jabuti <guilhermeoliveirfm@gmail.com>
2025-04-04 17:31:54 -0300

    v1

---
+++ Projeto 01/script.js
@@ -0,0 +1,40 @@
+let iene = document.querySelector("#valorIene");
+let realDolar = document.querySelector("#valorRealDolar");
+let selecaoMoedas = document.querySelector("#clicavel");
+let opcoesMoedas = document.querySelector("#opcoesMoedas");
+
+selecaoMoedas.addEventListener("click", () => {
+    if (opcoesMoedas.className === "") {
+        selecaoMoedas.addEventListener("click", () => {
+            opcoesMoedas.classList.add("active");
+        })
+    }
+    if (opcoesMoedas.className === "active") {
+        selecaoMoedas.addEventListener("click", () => {
+            opcoesMoedas.classList.remove("active");
+        })
+    }
+})
+
+
+
+
+
+/*
+    let ienes = document.querySelector('#Iene');
+    let reais = document.querySelector('#Real');
+    let ieneR = 0.04;
+    let realI = 25;
+
+    ienes.addEventListener("input", converterReal);
+    reais.addEventListener("input", converterIene);
+
+    function converterReal() {
+        const valor1 = parseFloat(ienes.value);
+        document.getElementById("Real").value = (valor1 * ieneR).toFixed(2);
+    }
+    function converterIene() {
+        const valor2 = parseFloat(reais.value);
+        document.getElementById("Iene").value = (valor2 * realI).toFixed(2);
+    }
+*/
